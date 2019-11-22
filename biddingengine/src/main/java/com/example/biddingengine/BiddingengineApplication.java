8298ce406faad2d37e050dacc2b4a49ffab02518
Wuxian Zhang <wuxian@tenmax.io>
2019-11-22 16:07:40 +0800

    first commit

---
+++ biddingengine/src/main/java/com/example/biddingengine/BiddingengineApplication.java
@@ -0,0 +1,13 @@
+package com.example.biddingengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiddingengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiddingengineApplication.class, args);
+	}
+
+}
---
+++ biddingengine/src/test/java/com/example/biddingengine/BiddingengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.biddingengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiddingengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
